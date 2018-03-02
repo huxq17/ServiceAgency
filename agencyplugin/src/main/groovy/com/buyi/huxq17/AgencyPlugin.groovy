b9790999f4b7a4f3cd21bbb688e85699151e33f7
huxq17 <huxq17@163.com>
2018-03-02 18:06:20 +0800

    update

--- agencyplugin/src/main/groovy/com/buyi/huxq17/AgencyPlugin.groovy
+++ agencyplugin/src/main/groovy/com/buyi/huxq17/AgencyPlugin.groovy
@@ -1,17 +1,14 @@
 package com.buyi.huxq17
 
 import com.android.build.gradle.AppExtension
-import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 public class AgencyPlugin implements Plugin<Project> {
 
     void apply(Project project) {
-        if (project.plugins.hasPlugin(AppPlugin)) {
-            def android = project.extensions.getByType(AppExtension)
-            def agencyTransform = new AgencyTransform(project)
-            android.registerTransform(agencyTransform)
-        }
+        def android = project.extensions.getByType(AppExtension)
+        def agencyTransform = new AgencyTransform(project)
+        android.registerTransform(agencyTransform)
     }
 }
